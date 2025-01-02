95b07607e73041375c4cebe469db719cd6922bc5
Preveen369 <spreveen123@gmail.com>
2025-01-02 19:47:39 +0530

    Problem no --> 50

---
+++ Basics/PowerofX.java
@@ -0,0 +1,21 @@
+public class PowerofX {
+    public double myPow(double x, int n) {
+        if(n < 0){
+            n = -n;
+            x = 1 / x;
+        }
+        double pow = 1;
+        while(n!=0){
+            if((n&1)!=0)
+                pow*=x;
+            x*=x;
+            n>>>=1;
+        }
+        return pow;
+    }
+    public static void main(String[] args) {
+        PowerofX pow = new PowerofX();
+        double res = pow.myPow(3, 10);
+        System.out.println(res);
+    }
+}
